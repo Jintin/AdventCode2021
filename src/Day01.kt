923fb5ac4ce7f4373b461fec40d2ddac5270689c
Jintin <jintin.lin1018@gmail.com>
2021-12-01 18:11:36 +0800

    add day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,30 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var count = 0
+        input.map { it.toInt() }
+            .reduce { a, b ->
+            if (b > a) {
+                count++
+            }
+            b
+        }
+        return count
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var count = 0
+
+        for (i in 3 until input.size) {
+            if (input[i].toInt() > input[i - 3].toInt()) {
+                count++
+            }
+        }
+        return count
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
 
     val input = readInput("Day01")
     println(part1(input))
